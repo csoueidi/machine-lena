e38f981ebd60269f406c108824ae7951896d5ae4
Chukri Soueidi <chukrisoueidi@gmail.com>
2023-12-01 09:23:58 +0100

    Update stepper.py

--- stepper/stepper.py
+++ stepper/stepper.py
@@ -203,6 +203,8 @@ class Stepper:
         self.timer.start()
 
     def stop(self):
+        if not self.running:
+            return
         self.running = False
         if self.timer is not None and self.timer.is_alive():
             self.timer.join()
